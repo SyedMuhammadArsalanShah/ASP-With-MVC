9ea0127328844c698cd182f00ac3e4ea460b5067
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-04-04 17:13:00 +0500

    code updated

---
+++ Lecture04Model/Lecture04Model/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Lecture04Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lecture04Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            Student obj = new Student()
+            {
+                id = 1,
+                name = "Laiba",
+                age = "20",
+                contact = "03031234567"
+            };
+
+
+
+
+            return View(obj);
+        }
+    }
+}
---
+++ Lecture04Model/Lecture04Model/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lecture04Model.MvcApplication" Language="C#" %>
