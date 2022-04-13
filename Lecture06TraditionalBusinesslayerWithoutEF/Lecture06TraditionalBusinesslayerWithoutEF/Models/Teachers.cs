dcc6a20594f37b5521b8e9c90e02d1ed89c7e8ed
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-04-13 17:49:54 +0500

    code updated

---
+++ Lecture06TraditionalBusinesslayerWithoutEF/Lecture06TraditionalBusinesslayerWithoutEF/Models/Teachers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lecture06TraditionalBusinesslayerWithoutEF.Models
+{
+    public class Teachers
+      {
+        public int Id { get; set;}
+        public string name { get; set;}
+      
+        public string contact { get; set; }
+        public int salary
+        {
+            get; set;
+        }
+
+    }
+}
---
+++ Lecture06TraditionalBusinesslayerWithoutEF/Lecture06TraditionalBusinesslayerWithoutEF/Models/TraditonalLayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Lecture06TraditionalBusinesslayerWithoutEF.Models
+{
+    public class TraditonalLayer
+    {
+
+
+        public IEnumerable<Teachers> teachers
+        {
+            get
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["CS"].ConnectionString;
+                List<Teachers> teachers = new List<Teachers>();
+
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand("select * from info " ,con);
+                    con.Open();
+                    SqlDataReader dataReader = command.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        Teachers teacher = new Teachers();
+
+                        teacher.Id      = Convert.ToInt32(dataReader["Id"]);
+                        teacher.name    = dataReader["Name"].ToString();
+                        teacher.contact = dataReader["contact"].ToString();
+                        teacher.salary  = Convert.ToInt32(dataReader["salary"]);
+                        teachers.Add(teacher);
+
+
+                    }
+
+                }
+
+                return teachers;
+            }
+
+        }
+
+    }
+}
