b9935ee32e50ef078028ef89b8bfd77328b586e0
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-04-22 17:57:38 +0500

    code updated

---
+++ Lecture10ExistingDatabase/Lecture10ExistingDatabase/Controllers/infoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lecture10ExistingDatabase.Models;
+
+namespace Lecture10ExistingDatabase.Controllers
+{
+    public class infoesController : Controller
+    {
+        private WatchesEntities db = new WatchesEntities();
+
+        // GET: infoes
+        public ActionResult Index()
+        {
+            return View(db.infoes.ToList());
+        }
+
+        // GET: infoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            info info = db.infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // GET: infoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: infoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,price")] info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.infoes.Add(info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(info);
+        }
+
+        // GET: infoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            info info = db.infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: infoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,price")] info info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(info).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(info);
+        }
+
+        // GET: infoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            info info = db.infoes.Find(id);
+            if (info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(info);
+        }
+
+        // POST: infoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            info info = db.infoes.Find(id);
+            db.infoes.Remove(info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Lecture10ExistingDatabase/Lecture10ExistingDatabase/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lecture10ExistingDatabase.Startup))]
+namespace Lecture10ExistingDatabase
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
