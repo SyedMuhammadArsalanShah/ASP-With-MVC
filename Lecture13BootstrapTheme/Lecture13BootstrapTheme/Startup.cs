05b441ae7998d63a2ba38c42eb4c0e785dda928b
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-05-25 20:16:52 +0500

    theme

---
+++ Lecture13BootstrapTheme/Lecture13BootstrapTheme/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lecture13BootstrapTheme.Startup))]
+namespace Lecture13BootstrapTheme
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
