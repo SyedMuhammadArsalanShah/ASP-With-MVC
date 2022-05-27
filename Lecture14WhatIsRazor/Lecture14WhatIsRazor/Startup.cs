70068a4c9602a829e5d30b93a052c16b3c5df596
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-05-27 21:44:46 +0500

    razor

---
+++ Lecture14WhatIsRazor/Lecture14WhatIsRazor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lecture14WhatIsRazor.Startup))]
+namespace Lecture14WhatIsRazor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
