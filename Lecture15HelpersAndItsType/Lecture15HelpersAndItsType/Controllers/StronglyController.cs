db94df69b3a1cc034d7160890e62309b0665fe01
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-06-06 21:02:55 +0500

    lecture updated

---
+++ Lecture15HelpersAndItsType/Lecture15HelpersAndItsType/Controllers/StronglyController.cs
@@ -0,0 +1,25 @@
+﻿using Lecture15HelpersAndItsType.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lecture15HelpersAndItsType.Controllers
+{
+    public class StronglyController : Controller
+    {
+        // GET: Strongly
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Info info)
+        {
+            return View();
+        }
+    }
+}
---
+++ Lecture15HelpersAndItsType/Lecture15HelpersAndItsType/Models/Info.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lecture15HelpersAndItsType.Models
+{
+    public class Info
+    {
+
+        public string misbah { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+
+
+
+    }
+}
