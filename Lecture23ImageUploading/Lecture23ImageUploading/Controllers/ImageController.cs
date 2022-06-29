2b78043c4c46d7c64c105461f85395616333c577
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-06-29 20:55:22 +0500

    new lecture img

---
+++ Lecture23ImageUploading/Lecture23ImageUploading/Controllers/ImageController.cs
@@ -0,0 +1,69 @@
+﻿using Lecture23ImageUploading.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lecture23ImageUploading.Controllers
+{
+    public class ImageController : Controller
+    {
+
+        image_finalEntities db = new image_finalEntities();
+        // GET: Image
+        public ActionResult Index()
+        {
+            return View(db.img_table.ToList());
+        }
+
+
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase file ,img_table mi)
+        {
+            string filename = Path.GetFileName(file.FileName);
+            string _filename = DateTime.Now.ToString("yymmssfff") + filename;
+            string extension = Path.GetExtension(file.FileName);
+            string path = Path.Combine(Server.MapPath("~/images/"),_filename);
+            mi.img = "~/images/" + _filename;
+            if (extension.ToLower()==".jpg" || extension.ToLower() == ".jpeg"|| extension.ToLower() == ".png")
+            {
+                if(file.ContentLength <= 1000000)
+                {
+                    db.img_table.Add(mi);
+                    if(db.SaveChanges()>=0)
+                    {
+                        file.SaveAs(path);
+                        ViewBag.msg = "Record Added";
+                        ModelState.Clear();
+
+                    }
+
+
+                }
+                else
+                {
+                    ViewBag.msg = "oops size is overflow";
+
+
+                } 
+
+
+
+
+            }
+
+
+
+
+            return View();
+        }
+    }
+}
---
+++ Lecture23ImageUploading/Lecture23ImageUploading/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lecture23ImageUploading")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lecture23ImageUploading")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ceb490e7-79f9-4268-89b0-9372e2d39cd0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
