8d664cec5d5881993e4047c969e6f5f93d5d1a40
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-07-27 20:57:30 +0500

    lecture uploaded

---
+++ Lecture26CreatebyAjax/Lecture26CreatebyAjax/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Lecture26CreatebyAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lecture26CreatebyAjax.Controllers
+{
+    public class UserController : Controller
+    {
+        lrkEntities db = new lrkEntities();
+
+
+
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(info  emp )
+        {
+            if (ModelState.IsValid==true)
+            {
+                db.infoes.Add(emp);
+               int a =db.SaveChanges();//
+                if (a>0)
+                {
+                    return Json("data inserted ");
+                }
+                else
+                {
+
+                    return Json("data is not inserted");
+                }           
+
+            }
+
+
+            return View();
+        }
+
+    }
+}
---
+++ Lecture26CreatebyAjax/Lecture26CreatebyAjax/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lecture26CreatebyAjax.Startup))]
+namespace Lecture26CreatebyAjax
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
